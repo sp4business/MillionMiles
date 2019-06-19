83af350a3f1c357419d41464f31a316db4e6c5de
sp4business <sp4business@gmail.com>
2019-06-19 17:13:12 -0400

    created reg and insurance component

---
+++ src/app/files/files.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilesComponent } from './files.component';
+
+describe('FilesComponent', () => {
+  let component: FilesComponent;
+  let fixture: ComponentFixture<FilesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FilesComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FilesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,58 +6,68 @@ import { ObdInstallComponent } from '../obd-install/obd-install.component';
 import { ObdLocateComponent } from '../obd-locate/obd-locate.component';
 import { ObdSettingComponent } from '../obd-setting/obd-setting.component';
 import { DiagnosticComponent } from '../diagnostic/diagnostic.component';
+import { FilesComponent } from '../files/files.component';
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  constructor(public modalCtrl: ModalController) {
+	constructor(public modalCtrl: ModalController) {
 
-  }
-  async presentDiagnostic() {
-	const benefitModal = await this.modalCtrl.create({
-		component: DiagnosticComponent,
-		componentProps: {test: 123},
-	});
-	return await benefitModal.present();
-}
-  async presentObdBenefit() {
-  	const benefitModal = await this.modalCtrl.create({
-  		component: ObdBenefitComponent,
-  		componentProps: {test: 123},
-  	});
-  	return await benefitModal.present();
-  }
+	}
+	async presentFiles() {
+		const filesModal = await this.modalCtrl.create({
+			component: FilesComponent,
+			componentProps: { test: 123 },
+		});
+		return await filesModal.present();
+	}
 
-    async presentObdContact() {
-  	const contactModal = await this.modalCtrl.create({
-  		component: ObdContactComponent,
-  		componentProps: {test: 123}
-  	});
-  	return await contactModal.present();
-  }
-    async presentObdInstall() {
-  	const installMdoal = await this.modalCtrl.create({
-  		component: ObdInstallComponent,
-  		componentProps: {test: 123}
-  	});
-  	return await installMdoal.present();
-  }
-    async presentObdLocate() {
-  	const benefitModal = await this.modalCtrl.create({
-  		component: ObdLocateComponent,
-  		componentProps: {test: 123}
-  	});
-  	return await benefitModal.present();
-  }
 
-    async presentObdSetting() {
-  	const benefitModal = await this.modalCtrl.create({
-  		component: ObdSettingComponent,
-  		componentProps: {test: 123}
-  	});
-  	return await benefitModal.present();
-  }
+	async presentDiagnostic() {
+		const benefitModal = await this.modalCtrl.create({
+			component: DiagnosticComponent,
+			componentProps: { test: 123 },
+		});
+		return await benefitModal.present();
+	}
+	async presentObdBenefit() {
+		const benefitModal = await this.modalCtrl.create({
+			component: ObdBenefitComponent,
+			componentProps: { test: 123 },
+		});
+		return await benefitModal.present();
+	}
+
+	async presentObdContact() {
+		const contactModal = await this.modalCtrl.create({
+			component: ObdContactComponent,
+			componentProps: { test: 123 }
+		});
+		return await contactModal.present();
+	}
+	async presentObdInstall() {
+		const installMdoal = await this.modalCtrl.create({
+			component: ObdInstallComponent,
+			componentProps: { test: 123 }
+		});
+		return await installMdoal.present();
+	}
+	async presentObdLocate() {
+		const benefitModal = await this.modalCtrl.create({
+			component: ObdLocateComponent,
+			componentProps: { test: 123 }
+		});
+		return await benefitModal.present();
+	}
+
+	async presentObdSetting() {
+		const benefitModal = await this.modalCtrl.create({
+			component: ObdSettingComponent,
+			componentProps: { test: 123 }
+		});
+		return await benefitModal.present();
+	}
 }
