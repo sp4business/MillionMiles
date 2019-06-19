a7266a2ce937a4d6e92b63c6134870423a47c7c7
sp4business <sp4business@gmail.com>
2019-06-19 16:17:25 -0400

    Finished Send Diagnostic Report Page

--- src/app/diagnostic/diagnostic.component.ts
+++ src/app/diagnostic/diagnostic.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-diagnostic',
@@ -8,12 +8,24 @@ import { ModalController } from '@ionic/angular';
 })
 export class DiagnosticComponent implements OnInit {
 
-  constructor(public modalCtrl: ModalController) { }
+  constructor(public modalCtrl: ModalController, public toastController: ToastController) { }
 
   ngOnInit() {}
 
-  dismiss(){
+  dismiss() {
     this.modalCtrl.dismiss();
   } 
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      //header: 'Toast Header',
+      message: 'Diagnostics Delivered',
+      duration: 2000,
+      position: 'bottom'
+    });
+    this.modalCtrl.dismiss();
+    toast.present();
+    
+  }
 
 }
