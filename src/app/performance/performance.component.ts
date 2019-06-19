4d5f9b8b15833f44f94a9eecba6f0a53d37acfc1
Kevin Li <lixinwei120@gmail.com>
2019-06-19 10:40:20 -0400

    made minor css change

---
+++ src/app/performance/performance.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-performance',
+  templateUrl: './performance.component.html',
+  styleUrls: ['./performance.component.scss'],
+})
+export class PerformanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
